d1b52d17378f4297a236a0192917e476b73eaaad
CaioMiranda12 <caioclm04@gmail.com>
2024-02-26 19:32:21 -0300

    aula 9  commit

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
 
 import { CategoriesRepository } from '../database/repositories/categories.repository';
+import { CreateCategoryDTO } from '../dtos/categories.dto';
 import { CategoryModel } from '../schemas/category.schema';
 import { CategoriesServices } from '../services/categories.services';
 
 export class CategoriesController {
-  async create(_: Request, res: Response) {
+  async create(
+    req: Request<unknown, unknown, CreateCategoryDTO>,
+    res: Response,
+  ) {
+    const { title, color } = req.body;
+
     const repository = new CategoriesRepository(CategoryModel);
     const service = new CategoriesServices(repository);
 
-    const result = await service.create();
+    const result = await service.create({ title, color });
 
     return res.status(201).json(result);
   }
