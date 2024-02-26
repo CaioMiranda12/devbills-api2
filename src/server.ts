06d46511ae145ddf431920d8c87df4cfa92f65b0
CaioMiranda12 <caioclm04@gmail.com>
2024-02-26 20:34:17 -0300

    aula 10

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { json } from 'express';
 
 import { setupMongo } from './database';
+import { ErrorHandler } from './middlewares/error-handler.middleware';
 import { routes } from './routes/index';
 
 setupMongo().then(() => {
@@ -9,6 +10,7 @@ setupMongo().then(() => {
 
   app.use(json());
   app.use(routes);
+  app.use(ErrorHandler);
 
   app.listen(3333, () => console.log('⚡ App is running at port 3333'));
 });
