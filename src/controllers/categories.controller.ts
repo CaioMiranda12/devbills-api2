1c4d4167d2207f79755da4bfe2e2eea98b64e656
CaioMiranda12 <caioclm04@gmail.com>
2024-02-28 19:58:11 -0300

    aula 13

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,42 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { z } from 'zod';
 
-import { CategoriesRepository } from '../database/repositories/categories.repository';
 import { CreateCategoryDTO } from '../dtos/categories.dto';
-import { CategoryModel } from '../schemas/category.schema';
 import { CategoriesServices } from '../services/categories.services';
 
 export class CategoriesController {
-  async create(
+  constructor(private categoriesService: CategoriesServices) {}
+
+  create = async (
     req: Request<unknown, unknown, CreateCategoryDTO>,
     res: Response,
     next: NextFunction,
-  ) {
+  ) => {
     try {
       const { title, color } = req.body;
 
-      const repository = new CategoriesRepository(CategoryModel);
-      const service = new CategoriesServices(repository);
-
-      const result = await service.create({ title, color });
+      const result = await this.categoriesService.create({ title, color });
 
       return res.status(StatusCodes.CREATED).json(result);
     } catch (err) {
       next(err);
     }
-  }
+  };
 
-  async index(req: Request, res: Response, next: NextFunction) {
+  index = async (_: Request, res: Response, next: NextFunction) => {
     try {
-      const repository = new CategoriesRepository(CategoryModel);
-      const service = new CategoriesServices(repository);
-
-      const result = await service.index();
+      const result = await this.categoriesService.index();
 
       return res.status(StatusCodes.OK).json(result);
     } catch (err) {
       next(err);
     }
-  }
+  };
 }
