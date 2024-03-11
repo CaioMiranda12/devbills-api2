4aad3a140dcf88f8ed1d14d556e5bd8ce2065c2b
CaioMiranda12 <caioclm04@gmail.com>
2024-03-11 20:12:56 -0300

    aula 19 commit

--- src/routes/transactions.route.ts
+++ src/routes/transactions.route.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
 import { TransactionsController } from '../controllers/transactions.controller';
-import { createTransitionSchema } from '../dtos/transactions.dto';
+import {
+  createTransitionSchema,
+  getDashboardSchema,
+  indexTransactionSchema,
+} from '../dtos/transactions.dto';
 import { TransactionsFactory } from '../factories/transactions.factory';
 import { ParamsType, validator } from '../middlewares/validator.middleware';
 
@@ -20,4 +24,20 @@ transactionsRoutes.post(
   controller.create,
 );
 
-transactionsRoutes.get('/', controller.index);
+transactionsRoutes.get(
+  '/',
+  validator({
+    schema: indexTransactionSchema,
+    type: ParamsType.QUERY,
+  }),
+  controller.index,
+);
+
+transactionsRoutes.get(
+  '/dashboard',
+  validator({
+    schema: getDashboardSchema,
+    type: ParamsType.QUERY,
+  }),
+  controller.getDashboard,
+);
