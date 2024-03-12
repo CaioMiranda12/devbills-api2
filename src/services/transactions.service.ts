fc852fb8a607922e1fcd619b489d68efeef1e45d
CaioMiranda12 <caioclm04@gmail.com>
2024-03-12 20:15:21 -0300

    aula 20 commit

--- src/services/transactions.service.ts
+++ src/services/transactions.service.ts
@@ -8,6 +8,7 @@ import {
   IndexTransactionDTO,
 } from '../dtos/transactions.dto';
 import { Balance } from '../entities/balance.entity';
+import { Expense } from '../entities/expense.entity';
 import { Transaction } from '../entities/transactions.entity';
 import { AppError } from '../errors/app.error';
 
@@ -51,11 +52,20 @@ export class TransactionsService {
     return transactions;
   }
 
-  async getDashboard({ beginDate, endDate }: GetDashboardDTO) {
-    let balance = await this.transactionsRepository.getBalance({
-      beginDate,
-      endDate,
-    });
+  async getDashboard({
+    beginDate,
+    endDate,
+  }: GetDashboardDTO): Promise<{ balance: Balance; expenses: Expense[] }> {
+    let [balance, expenses] = await Promise.all([
+      this.transactionsRepository.getBalance({
+        beginDate,
+        endDate,
+      }),
+      this.transactionsRepository.getExpenses({
+        beginDate,
+        endDate,
+      }),
+    ]);
 
     if (!balance) {
       balance = new Balance({
@@ -66,6 +76,6 @@ export class TransactionsService {
       });
     }
 
-    return balance;
+    return { balance, expenses };
   }
 }
